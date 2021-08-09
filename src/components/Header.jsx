9196e036ac2b40e45380a6390556100a94ee671d
Russen <russen333@gmail.com>
2021-08-09 08:35:00 +0300

    12 react - creat components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+
+	return (
+		<header className='header'>
+			<img src='https://www.mintformations.co.uk/blog/wp-content/uploads/2020/05/shutterstock_583717939.jpg' />
+		</header>
+	)
+}
+
+export default Header;
