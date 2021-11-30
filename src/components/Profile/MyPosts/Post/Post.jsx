32da343731070ed893a902e0c40b464d2fd20631
Russen <russen333@gmail.com>
2021-11-30 11:54:42 +0200

    рефакторинг img

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,5 +1,6 @@
 import s from './Post.module.css';
 import React from 'react';
+import avatar from './../../../../assets/img/images.jpg'
 const Post = (props) => {
 
 	return (
@@ -7,7 +8,7 @@ const Post = (props) => {
 
 		<li className={s.item}>
 			<div className={s.box}>
-				<img className={s.img} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhnOH5qHwy79ZR9qZgsvAcuVMFJE3jN6CCpg&usqp=CAU" alt="avatar" />
+				<img className={s.img} src={avatar} alt="avatar" />
 				{props.message}
 			</div>
 			<div>
